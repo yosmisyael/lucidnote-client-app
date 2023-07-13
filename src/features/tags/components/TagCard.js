b8b5823c8e942fae1b5379faca2b1fe823f773f3
yosmisyael <yosmisyael7308@gmail.com>
2023-07-13 08:20:39 +0700

    make update tag request to server

---
+++ src/features/tags/components/TagCard.js
@@ -0,0 +1,66 @@
+import { MdOutlineModeEdit, MdOutlineClose, MdOutlineCheck } from 'react-icons/md'
+import { BsTrash } from 'react-icons/bs'
+import tags from '../assets/tagCard.module.css'
+import { useState, useRef, useEffect } from 'react'
+
+const TagCard = ({ id, tagName }) => {
+  const [editMode, setEditMode] = useState(false)
+  
+  const triggerEditMode = () => {
+    setEditMode(!editMode)
+    console.log(inputTagRef.current.id)
+  }
+
+  const inputTagRef = useRef()
+  useEffect(() => {
+    if (editMode) {
+      inputTagRef.current.focus();
+    }
+  }, [editMode]);
+
+  const commitEdit = async () => {
+    const tagId = inputTagRef.current.getAttribute('data-id')
+    const tagValue = inputTagRef.current.value
+    try {
+      const response = await fetch(`http://localhost:3100/api/tags/${tagId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+          tagName: tagValue
+        })
+      })
+      if (response.status !== 200) {
+        const { errors } = await response.json()
+        throw new Error(errors)
+      }
+      setEditMode(!editMode)
+    } catch (error) {
+      console.log(error.message)
+    }
+  } 
+
+  return (
+    <div className={tags.wrapper}>
+      <div className={tags.tagWrapper}>
+        <input ref={inputTagRef} type="text" defaultValue={tagName} data-id={id} disabled={ editMode ? false : true } style={editMode ? {cursor: 'text'} : {cursor: 'pointer'}} />
+        <div className={tags.actionBtn}>
+          { !editMode && (
+            <span onClick={triggerEditMode}><MdOutlineModeEdit size={20}/></span>
+          ) }
+          { editMode && (
+            <span onClick={triggerEditMode}><MdOutlineClose size={20}/></span>
+          ) }
+          { editMode && (
+            <span onClick={commitEdit}><MdOutlineCheck size={20}/></span>
+          ) }
+          <span><BsTrash size={20}/></span>
+        </div>
+      </div>
+    </div>
+  )
+}
+ 
+export default TagCard;
--- src/features/tags/index.js
+++ src/features/tags/index.js
@@ -1,13 +1,14 @@
-import { useNavigate } from 'react-router-dom';
-import tags from './assets/tags.module.css';
-import TagModal from './components/tagModal';
-import { MdOutlineArrowForwardIos, MdOutlineModeEdit, MdAddHome } from 'react-icons/md';
-import { BsTags, BsTrash } from 'react-icons/bs';
-import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+import tags from './assets/tags.module.css'
+import TagModal from './components/tagModal'
+import TagCard from './components/TagCard'
+import { MdOutlineArrowForwardIos, MdAddHome } from 'react-icons/md'
+import { BsTags } from 'react-icons/bs'
+import { useState, useEffect } from 'react'
 
 const TagsFeature = () => {
-  const navigate = useNavigate();
-  const [tagConfigurationModal, setTagConfigurationModal] = useState(false);
+  const navigate = useNavigate()
+  const [tagConfigurationModal, setTagConfigurationModal] = useState(false)
   const handlerModal = () => {
     setTagConfigurationModal(!tagConfigurationModal)
   }
@@ -39,22 +40,14 @@ const TagsFeature = () => {
         <h1>Tag List <BsTags /></h1>
       </div>
       { tagList.map(tag => (
-        <div key={ tag.id } className={tags.wrapper}>
-          <div className={tags.tagWrapper}>
-            <h2>{ tag.tagName }</h2>
-            <div className={tags.actionBtn}>
-              <span><MdOutlineModeEdit size={20}/></span>
-              <span><BsTrash size={20}/></span>
-            </div>
-          </div>
-        </div>
+        <TagCard key={ tag.id } id={ tag.id } tagName={ tag.tagName }/>
       ))}
       <div className={tags.btnAdd} onClick={handlerModal}> 
         <MdAddHome size={24}/> Add Tag
       </div>
       { tagConfigurationModal && <TagModal handlerModal={ handlerModal }/> }
     </section>
-  );
+  )
 }
  
-export default TagsFeature;
+export default TagsFeature
