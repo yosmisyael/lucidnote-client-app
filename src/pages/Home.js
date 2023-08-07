ebf71d0d83fb78e8ac6aa6ccec4a4245ca274856
unknown <yosmisyael7308@gmail.com>
2023-08-07 11:16:56 +0700

    style: update homepage ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import home from '../assets/styles/home.module.css';
+import style from '../assets/styles/home.module.css';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Button from '../components/Button';
@@ -9,43 +9,31 @@ import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const navigate = useNavigate()
+  const holes = []
+  for (let i = 0; i < 18; i++) {
+    holes.push(<div key={i} className={style.hole}></div>)
+  }
   return (
     <React.Fragment>
       <Navbar />
-      <section className={`${home.canvas} relative`}>
-        <div className={home.holeContainer}>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
-          <div className={home.hole}></div>
+      <section className={`${style.canvas} relative`}>
+        <div className={style.holeContainer}>
+          {holes}
         </div>
-        <div className={home.lineContainer}>
-          <div className={home.line}></div>
+        <div className={style.lineContainer}>
+          <div className={style.line}></div>
         </div>
-        <div className={home.col1}>
+        <div className={style.col1}>
           <h1><u>Capture</u>, <u>Organize</u>, and <u>Unleash</u> Your Ideas!</h1>
           <p>
             Keep capturing your ideas with this app full of creativity<FcIdea /> and productivity<FcBullish />
           </p>
-          <div className={`${home.btnWrapper} flex flex-centered`}>
+          <div className={style.btnWrapper}>
             <Button buttonName='Get Started' buttonType='primary' func={() => navigate('/authentication')} />
           </div>
         </div>
-        <div className={home.col2}>
-          <img src={homepageIcon} height={'600rem'} alt="" />
+        <div className={style.col2}>
+          <img src={homepageIcon} className={style.homepageIcon} alt="" />
         </div>
       </section>
       <Footer />
