dde6aceb3124ba69f34e4b97b82774cb41460fe7
Yosevian Wiarda <yosmisyael7308@gmail.com>
2023-08-14 23:29:05 +0700

    fix: fix auth context functionality

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -4,27 +4,7 @@ export const AuthContext = createContext()
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
-  const token = localStorage.getItem('token')
-
-  useEffect(() => {
-    if (!token) {
-      setUser(null)
-    }
-
-    const getUser = async () => {
-      const response = await fetch('http://localhost:3100/api/users/current', {
-        method: 'GET',
-        headers: {
-          'Authorization': localStorage.getItem('token'),
-        } 
-      })
-      const { data } = await response.json()
-      setUser(data)
-    }
-    
-    getUser()
-  }, [token])
-
+  
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       {children}
